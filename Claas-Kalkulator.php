b3e8594cc05a5ab5db7e38e838b85b8ad7f1fe6b
NaauffL <62578983+NaauffL@users.noreply.github.com>
2020-03-24 08:58:08 +0700

    Membuat class Kalkulator

---
+++ Claas-Kalkulator.php
@@ -0,0 +1,20 @@
+<?php
+
+class Calculator {
+  protected $angka1, $angka2, $operator, $hasil;
+  
+  public function setAngka($angka1, $angka2) {
+      $this->angka1 = $angka1;
+      $this->angka2 = $angka2;
+      }
+      
+  public function setOperator($operator) {
+      $this->operator = $operator
+      }
+      
+  public function getHasil() {
+      return $this->output;
+      }
+ }
+
+?>
